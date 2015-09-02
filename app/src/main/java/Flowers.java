460a6a1b44052304ce7205b67e1df6cb20208abe
Brandon <brhaskett@yahoo.com>
2015-09-01 23:10:35 -0400

    creating a flower object to hold info from DB

---
+++ app/src/main/java/Flowers.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Created by Brandon on 9/1/2015.
+ */
+public class Flowers
+{
+    public Flowers()
+    {
+
+    }
+
+    static String FlowerName;
+    static String Indications;
+    static List<String> Tabs;
+}
